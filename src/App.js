0e300d204168e024564dcaf3c784cbd9a5aadada
Fernando <fernandojavierge@gmail.com>
2024-06-24 23:35:56 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import BasicForm from "./components/BasicForm";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+    
+        <h3>
+          Login
+        </h3>
+    
+       <BasicForm />
+    </div>
+  );
+}
+
+export default App;
